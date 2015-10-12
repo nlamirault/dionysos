6b413644c8f474458955e996309ddb53e511e2b3
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-10-12 15:23:37 +0200

    Cleanup project
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- .dir-locals.el
+++
@@ -1,6 +0,0 @@
-((nil
-  (bug-reference-url-format . "https://github.com/nlamirault/dionysos/issues/%s"))
- (markdown-mode
-  (bug-reference-bug-regexp . "\\[GH-\\(?2:[[:digit:]]+\\)]")))
- (rst-mode
-  (bug-reference-bug-regexp . "\\[GH-\\(?2:[[:digit:]]+\\)]")))
